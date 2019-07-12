a4bc311a8a0a504f343520b89e530783da01d026
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-12 23:58:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+  value_to_find = 6
+  array.index(value_to_find)    
+  end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort.pop
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort.
 end
